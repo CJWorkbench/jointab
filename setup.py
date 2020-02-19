4257b71aa006398d6a1635d570de6ff965e08183
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-19 11:07:28 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="jointab",
+    version="0.0.1",
+    description="Join data using shared columns between two tabs",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/jointab",
+    packages=[""],
+    py_modules=["jointab"],
+    install_requires=["pandas==0.25.0"],
+)
